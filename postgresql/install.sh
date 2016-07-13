abc95e73fd12bf22bd0f3cd5fe8428bbe0c0f740
Wesley Hoffman <wesleyhoffman109@gmail.com>
2016-07-13 18:21:29 -0500

    Update with new atom packages and remove applications I no longer use

--- postgresql/install.sh
+++
@@ -1,5 +0,0 @@
-brew install postgresql
-
-mkdir -p ~/Library/LaunchAgents
-
-cp /usr/local/Cellar/postgresql/9.5.0/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/
--- spark/path.zsh
+++
@@ -1,1 +0,0 @@
-export PATH="/Users/wesleyhoffman/Applications/spark-1.5.2-hadoop2.6/bin:$PATH"
